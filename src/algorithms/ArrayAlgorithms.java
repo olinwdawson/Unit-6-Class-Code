56edb54d3393b507e8467cfb24b0815007c68e59
smithbwlhs <smithb@wlwv.k12.or.us>
2024-12-11 14:07:09 -0800

    updated

---
+++ src/algorithms/ArrayAlgorithms.java
@@ -0,0 +1,40 @@
+package algorithms;
+
+public class ArrayAlgorithms {
+    public static void main(String[] args) {
+
+    }
+
+    // Finds the minimum value in an array
+    public static int findMin(int[] array) {
+        // TODO: Implement this method
+        return 0;
+    }
+
+    // Finds the maximum value in an array
+    public static int findMax(int[] array) {
+        // TODO: Implement this method
+        return 0;
+    }
+
+    // Finds the average value of elements in an array
+    public static double findAverage(int[] array) {
+        // TODO: Implement this method
+        return 0.0;
+    }
+
+    // Shifts the array to the left by one position
+    public static void shiftLeft(int[] array) {
+        // TODO: Implement this method
+    }
+
+    // Shifts the array to the right by one position
+    public static void shiftRight(int[] array) {
+        // TODO: Implement this method
+    }
+
+    // Reverses the elements in the array
+    public static void reverseArray(int[] array) {
+        // TODO: Implement this method
+    }
+}
--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -1,23 +1,21 @@
-package lesson1;
+package lesson2;
+
+import lesson1.Henry;
 
 public class Main {
     public static void main(String[] args) {
         //create with initial values
         int[] scores = {47,82,36,95,74,64,53,92};
 
-        //create with default values
-        String[] defaultScores = new String[8];
-        Henry[] listOHenrys = new Henry[10];
-        Henry latestHenry = new Henry(15,"Henry 2.0","Windows ME");
-
-        //change a value
-        listOHenrys[0] = latestHenry;
 
         //print scores
         for(int i = 0; i<scores.length; i++){
             System.out.print(scores[i] + " ");
         }
 
-
+        //using an enhanced for loop
+        for(int score:scores){
+            System.out.println(score);
+        }
     }
 }
